349d0c90d384aca6c61d331e894eeed021df2318
Scott Schipke <swschipke@gmail.com>
2019-10-09 15:44:23 -0600

    Add functionality to fetch characters by film
    Co-Authored By: Scott Schipke <swschipke@gmail.com>

--- src/Util/apiCalls.js
+++ src/Util/apiCalls.js
@@ -10,11 +10,36 @@ const sortFilmsByReleaseDate = (data) => {
     let yearA = parseInt(a.release_date.slice(0, 4));
     let yearB = parseInt(b.release_date.slice(0, 4));
     return yearA - yearB
-  }).map(movie => {
+  }).map((movie,i) => {
     return {
       title: movie.title,
+      id: i + 1,
       episode: movie.episode_id,
       releaseYear: movie.release_date.slice(0, 4)
     }
   })
 }
+
+export const getCharacters = (id) => {
+  let url = `https://swapi.co/api/films/${id}`
+  return fetch(url).then(res => res.json())
+  .then(data => data.characters)
+  .then(data => cleanCharacterData(data))
+
+}
+
+const cleanCharacterData = (characterUrls) => {
+  let charactersData = characterUrls.map(charUrl => {
+    return fetch(charUrl).then(res => res.json()).then(data => {
+      return {
+        name:data.name,
+        homeworld: data.homeworld,
+        species: data.species,
+        films: data.films,
+      }
+    })
+  })
+  console.log(charactersData)
+  return Promise.all(charactersData)
+}
+
