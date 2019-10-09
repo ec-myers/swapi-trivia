d0fbc60d501c13e75e7e5b2b0b709b2905808a48
Scott Schipke <swschipke@gmail.com>
2019-10-08 21:21:50 -0600

    Add error states to unfilled inputs in form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -21,15 +21,15 @@ class Form extends Component {
   handleSubmit = () => {
     if (!this.state.name) {
       this.setState({nameErr: true})
-    }
+    } else { this.setState({ nameErr: false }) }
 
     if (!this.state.quote) {
       this.setState({quoteErr: true})
-    }
+    } else { this.setState({ quoteErr: false })}
 
     if(!this.state.rank) {
       this.setState({rankErr: true})
-    }
+    } else { this.setState({ rankErr: false }) }
 
     if(this.state.name && this.state.quote && this.state.rank) {
       let user = {name:this.state.name, 
@@ -41,26 +41,41 @@ class Form extends Component {
 
   render() {
     const {name, quote, rank, nameErr, quoteErr, rankErr} = this.state
+    let nameClass = nameErr ? "error" : "";
+    let quoteClass = quoteErr ? "error" : "";
+    let rankClass = rankErr ? "error" : "";
+
+
     return(
       <form>
         <label htmlFor="name">Name</label>
         <input value={name} id="name" 
         placeholder="Enter a name"
+        className={nameClass}
         onChange={this.handleChange}/>
+        <div>
         {nameErr && <p>No Name</p>}
+        </div>
         <label htmlFor="quote">Favorite Star Wars Quote</label>
-        <input value={quote} id="quote" 
-          placeholder="Enter your favorite Quote"        
+        <input value={quote} id="quote"
+        className={quoteClass} 
+          placeholder="Enter your favorite Quote"  
         onChange={this.handleChange}/>
+        <div>
         {quoteErr && <p>No Quote</p>}
+        </div>
         <label htmlFor="rank">Rank</label>
-        <select value={rank} onChange={this.handleChange} id="rank">
+        <select value={rank} 
+        className={rankClass}
+        onChange={this.handleChange} id="rank">
           <option value="">Choose A Rank</option>
           <option value="Padawan">Padawan</option>
           <option value="Jedi Knight">Jedi Knight</option>
           <option value="Jedi Master">Jedi Master</option>
         </select>
+        <div>
         {rankErr && <p>No Rank</p>}
+        </div>
         <button type="button" onClick={ this.handleSubmit }>May The Force Be With You</button>
       </form>
     )
