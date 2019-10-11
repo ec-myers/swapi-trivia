c55061c7fe1dad995a4d4b085a0701509a81bd00
“Elyse <elysemyers1@gmail.com>
2019-10-10 22:45:02 -0600

    Adds styling for MovieCard component

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MovieCard.scss';
 
 const MovieCard = ({movie}) => {
   return(
-    <div className="movie-card">
+    <div className="Movie-Card">
       <h2>{movie.title}</h2>
-      <h3>{movie.episode}</h3>
-      <h3>{movie.releaseYear}</h3>
+      <h3>Episode: {movie.episode}</h3>
+      <h3>Release in {movie.releaseYear}</h3>
       <Link to={`/movies/${movie.id}`}>
         <button>Explore Characters</button> 
       </Link>
