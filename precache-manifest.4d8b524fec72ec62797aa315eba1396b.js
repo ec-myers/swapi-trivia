265843874999aed9696b8bd604a045133cceb446
“Elyse <elysemyers1@gmail.com>
2019-10-15 15:18:07 -0600

    Updates

---
+++ precache-manifest.4d8b524fec72ec62797aa315eba1396b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30671d014e6d8a721c1f916fd8a759b",
+    "url": "/swapi-trivia/index.html"
+  },
+  {
+    "revision": "03e5547eaebda14f158e",
+    "url": "/swapi-trivia/static/css/main.33e5d511.chunk.css"
+  },
+  {
+    "revision": "2de5ec59d9c59f80f4b1",
+    "url": "/swapi-trivia/static/js/2.3fe8a09c.chunk.js"
+  },
+  {
+    "revision": "03e5547eaebda14f158e",
+    "url": "/swapi-trivia/static/js/main.5fbddc3b.chunk.js"
+  },
+  {
+    "revision": "4d91dc8747153c95bca8",
+    "url": "/swapi-trivia/static/js/runtime-main.ac423d25.js"
+  },
+  {
+    "revision": "cd14ff079864d5dc9b84e3aad85aac5d",
+    "url": "/swapi-trivia/static/media/loadingImg.cd14ff07.gif"
+  },
+  {
+    "revision": "2b2bc62452d5218fe7d369852cba8f5c",
+    "url": "/swapi-trivia/static/media/spaceImg.2b2bc624.jpg"
+  },
+  {
+    "revision": "af239dd14fe8db5047967475a49f7b4a",
+    "url": "/swapi-trivia/static/media/tatooine_background.af239dd1.jpg"
+  }
+]);
