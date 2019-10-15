bc506f0ba4d56888b51ba2a3a97fc5bc5f8a27b8
Scott Schipke <swschipke@gmail.com>
2019-10-15 14:29:02 -0600

    Add styling for form and favorited cards
    Co-Authored By: Elyse Myers <elysemyers1@gmail.com>

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -3,19 +3,21 @@ import './CharacterCard.scss';
 
 const CharacterCard = ({character, toggleFavorite, favorites}) => {
   const { name, homeworld, population, species, films } = character;
+  let isFavorite = favorites.map(char => char.name).includes(name) ? 'favorite' : '' ;
+  let btnMessage = isFavorite ? 'Remove Favorite' : 'Add Favorite'
   const allFilms = films.map(film => {
   return <li>{film}</li>
   })
 
   return (
-    <div className="Character-Card">
+    <div className={`Character-Card ${isFavorite}`} >
       <h2>{name}</h2>
       <h3>Homeworld: {homeworld}</h3>
       <h3>Population: {population}</h3>
       <h3>Species: {species}</h3>
       <ul>Films: {allFilms}</ul>
       <div className='div-favorite'>
-        <button id={name} onClick={() => {toggleFavorite(character)}}>Favorite ({favorites.length})</button>
+        <button id={name} onClick={() => {toggleFavorite(character)}}>{btnMessage} ({favorites.length})</button>
       </div>
     </div>
   )
