00ca895a2b2a81b7cd0618a8decc6b3b46072509
“Elyse <elysemyers1@gmail.com>
2019-10-14 22:37:55 -0600

    Adds styling for CharacterCard component

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -15,11 +15,13 @@ const CharacterCard = ({character, toggleFavorite, favorites}) => {
   return (
     <div className="Character-Card">
       <h2>{name}</h2>
-      <h3>Homeworld:{homeworld}</h3>
-      <h3>Population:{population}</h3>
-      <h3>Species:{species}</h3>
-      <ul>Films:{allFilms}</ul>
-       <button id={name} onClick={() => {toggleFavorite(character)}}>Favorite ({favorites.length})</button>
+      <h3>Homeworld: {homeworld}</h3>
+      <h3>Population: {population}</h3>
+      <h3>Species: {species}</h3>
+      <ul>Films: {allFilms}</ul>
+      <div className='div-favorite'>
+        <button id={name} onClick={() => {toggleFavorite(character)}}>Favorite ({favorites.length})</button>
+      </div>
     </div>
   )
 }
