a0e64c6ace03cf07cc66842ddd43064ebb8f40eb
Scott Schipke <swschipke@gmail.com>
2019-10-14 21:04:49 -0600

    'Fix formatting in CharacterCard

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -2,13 +2,8 @@ import React from 'react';
 
 const CharacterCard = ({character, toggleFavorite, favorites}) => {
   const { name, homeworld, population, species, films } = character;
-
-  // if (favorites.some(favorite => favorite.name === name)) {
-  //  character.isFavorite = true
-  // }
-
   const allFilms = films.map(film => {
-   return <li>{film}</li>
+  return <li>{film}</li>
   })
 
   return (
@@ -18,7 +13,7 @@ const CharacterCard = ({character, toggleFavorite, favorites}) => {
       <h3>Population:{population}</h3>
       <h3>Species:{species}</h3>
       <ul>Films:{allFilms}</ul>
-       <button id={name} onClick={() => {toggleFavorite(character)}}>Favorite ({favorites.length})</button>
+      <button id={name} onClick={() => {toggleFavorite(character)}}>Favorite ({favorites.length})</button>
     </div>
   )
 }
